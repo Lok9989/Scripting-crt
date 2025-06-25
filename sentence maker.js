b2d6d3891b2a70b29c39aa7f508aeac5957b0259
Loksainath <yloksainath629@gmail.com>
2025-06-25 11:24:40 +0530

    free code camp

---
+++ sentence maker.js
@@ -0,0 +1,33 @@
+// Declare variables using let
+let adjective = "mighty";
+let noun = "dragon";
+let verb = "angry";
+let place = "volcano";
+let adjective2 = "fiery";
+let noun2 = "tacos";
+
+// Create first story using string concatenation
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + 
+                 " who loved to eat " + noun2 + ". The " + noun +
+                 " lived in a " + place + " and had " + adjective2 + 
+                 " nostrils that blew fire when it was " + verb + ".";
+
+// Output the first story
+console.log("First story: " + firstStory);
+
+// Reassign new values to variables
+adjective = "tiny";
+noun = "unicorn";
+verb = "sleepy";
+place = "rainbow forest";
+adjective2 = "sparkling";
+noun2 = "marshmallows";
+
+// Create second story
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + 
+                  " who loved to eat " + noun2 + ". The " + noun +
+                  " lived in a " + place + " and had " + adjective2 + 
+                  " nostrils that blew fire when it was " + verb + ".";
+
+// Output the second story
+console.log("Second story: " + secondStory);
